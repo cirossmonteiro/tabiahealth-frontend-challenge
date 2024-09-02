be7e54823f61125d3282040450c2dec109019cf5
Ciro Monteiro <cirossmonteiro@gmail.com>
2024-09-02 13:45:53 -0300

    responsivity improved

--- src/pages/comparison/index.tsx
+++ src/pages/comparison/index.tsx
@@ -5,11 +5,11 @@ import data from "../../data.json";
 
 const Page = () => {
   return (
-    <div className="w-100 p-5 page">
+    <div className="w-100 p-3 p-sm-5 page">
       <div className="mb-5">
         <span className="page-title">Comparison</span>
       </div>
-      <div className="p-5 page-body">
+      <div className="p-3 p-sm-5 page-body">
         <Table countries={data.countries} categories={data.categories as ICategory[]} data={data.data} />
       </div>
     </div>
