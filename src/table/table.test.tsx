40edb4a7f02ec7f273c73547f12d6e9c7488fecf
Ciro Monteiro <cirossmonteiro@gmail.com>
2024-09-02 18:43:42 -0300

    snapshot testing

--- src/table/table.test.tsx
+++ src/table/table.test.tsx
@@ -2,14 +2,22 @@ import {act} from 'react';
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer';
+
 import Table from './';
 
 test('renders learn react link', async () => {
-  render(<Table
-    categories={[{ name: "category", icon: ["solid", "house"] }]}
-    countries={["country"]}
-    data={[[10]]}
-  />);
+  const component = (
+    <Table
+      categories={[{ name: "category", icon: ["solid", "house"] }]}
+      countries={["country"]}
+      data={[[10]]}
+    />
+  );
+
+  render(component);
+
+  // testing sorting icons
   expect(screen.getByTestId('sort-icon').className).toBe("fa-solid fa-sort");
   userEvent.click(screen.getByTestId('sort-icon'));
   expect(screen.getByTestId('sort-icon').className).toBe("fa-solid fa-sort-up");
@@ -17,4 +25,8 @@ test('renders learn react link', async () => {
   expect(screen.getByTestId('sort-icon').className).toBe("fa-solid fa-sort-down");
   userEvent.click(screen.getByTestId('sort-icon'));
   expect(screen.getByTestId('sort-icon').className).toBe("fa-solid fa-sort");
+
+  const rendered = renderer.create(component);
+  let tree = rendered.toJSON();
+  expect(tree).toMatchSnapshot();
 });
